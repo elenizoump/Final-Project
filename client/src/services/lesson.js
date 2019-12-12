6fa874945064d53c7245fe1447a8d3b75aca8d2a
another-dj <alexis_forestier@hotmail.com>
2019-12-12 17:52:30 +0000

    lesson services

---
+++ client/src/services/lesson.js
@@ -0,0 +1,58 @@
+import axios from "axios";
+
+const apiService = axios.create({
+  // Before proxying
+  // baseURL: 'http://localhost:3020/api'
+  // After proxying
+  // baseURL: 'http://localhost:3000/api'
+  baseURL: "/api"
+});
+
+export const list = async () => {
+  try {
+    const response = await apiService.get("/list");
+    const lessons = response.data.lessons;
+    return lessons;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const load = async id => {
+  try {
+    const response = await apiService.get(`/${id}`);
+    const lesson = response.data.lesson;
+    return lesson;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const edit = async (id, lesson) => {
+  try {
+    await apiService.patch(`/${id}`, lesson);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const remove = async id => {
+  try {
+    await apiService.delete(`/${id}`);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const create = async lesson => {
+  const data = new FormData();
+  data.append("instrument", lesson.instrument);
+  data.append("hoursOfStudy", lesson.hoursOfStudy);
+
+  try {
+    const response = await apiService.post(`/create`, data);
+    return response.data.lesson;
+  } catch (error) {
+    throw error;
+  }
+};

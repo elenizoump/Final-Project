a682bf66e6821d313200bb06935297c773392d6c
another-dj <alexis_forestier@hotmail.com>
2019-12-16 18:27:37 +0000

    teacher profil

--- client/src/views/Teacher/TeacherProfileView.jsx
+++ client/src/views/Teacher/TeacherProfileView.jsx
@@ -88,21 +88,18 @@ class TeacherProfileView extends Component {
             </Card>
           </div>
         )}
-<<<<<<< HEAD
+
         <div className="UsersMapLocation">
-            < MapContainer />
+          <MapContainer />
         </div>
-=======
+
         {/* <div className="UsersMapLocation">
             < GoogleApiWrapper />
           Map goes here
         </div> */}
-<<<<<<< HEAD
-        <br />
-=======
->>>>>>> 23e076227895ece04eee5a7a1a1bccb816af3e8f
 
->>>>>>> 9f561d7677eecefc05e1a10c1eaf54597beb3a2d
+        <br />
+
         <button onClick={() => this.toggle()}>
           CLICK HERE TO SEE A POSSIBLE EDIT FORM ONCE ELENI GET IT
         </button>

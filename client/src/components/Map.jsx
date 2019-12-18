af83a91fda464f7f19d962fe4c396a846a51ea04
Eleni Zoumpoulaki <elenizoump@hotmail.com>
2019-12-18 19:11:55 +0000

    deployment changes

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
-import mapStyles from './MapStyles';
+import React, { Component } from "react";
+import mapStyles from "./MapStyles";
 
-
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 export class MapContainer extends Component {
   state = {
     showingInfoWindow: false,
     activeMarker: {},
-    selectedPlace: {},
+    selectedPlace: {}
   };
 
   onMarkerClick = (props, marker, e) =>
@@ -18,45 +17,45 @@ export class MapContainer extends Component {
       showingInfoWindow: true
     });
 
-  onMapClicked = (props) => {
+  onMapClicked = props => {
     if (this.state.showingInfoWindow) {
       this.setState({
         showingInfoWindow: false,
         activeMarker: null
-      })
+      });
     }
   };
 
   render() {
     return (
-      <Map initialCenter={{
-        lat: 38.7881531,
-        lng: -9.1124146
-      }}
+      <Map
+        initialCenter={{
+          lat: 38.7881531,
+          lng: -9.1124146
+        }}
         defaultOptions={{ styles: mapStyles }}
         defaultZoom={10}
         google={this.props.google}
-        onClick={this.onMapClicked}>
-
-
+        onClick={this.onMapClicked}
+      >
         <Marker
           onClick={this.onMarkerClick}
-          title={'Beta-I'}
-          name={'Beta-I'}
-          position={{ lat: 38.7881531, lng: -9.1124146 }} />
+          title={"Beta-I"}
+          name={"Beta-I"}
+          position={{ lat: 38.7881531, lng: -9.1124146 }}
+        />
 
         <InfoWindow
           marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}>
+          visible={this.state.showingInfoWindow}
+        >
           <div>
             <h1>{this.state.selectedPlace.name}</h1>
           </div>
         </InfoWindow>
       </Map>
-    )
+    );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: ('AIzaSyBr1pVA3qkMszfSNm2lMcpR4TbikezP4uE')
-})(MapContainer)
+export default GoogleApiWrapper;

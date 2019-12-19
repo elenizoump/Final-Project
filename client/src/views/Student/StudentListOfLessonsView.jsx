1a694f7029d78131524905f300dc558ccc03221d
Eleni Zoumpoulaki <elenizoump@hotmail.com>
2019-12-19 17:55:20 +0000

    small style fixes

--- client/src/views/Student/StudentListOfLessonsView.jsx
+++ client/src/views/Student/StudentListOfLessonsView.jsx
@@ -8,23 +8,21 @@ export default class StudentListOfLessonsView extends Component {
     const lessonList = this.props.lessons;
     return (
       <div className="listOfLessons">
+        <h2>Upcoming Lessons</h2>
         <div id="listOfLessonsGroup">
           <ListGroup className="overflow-auto">
-            <h2>Upcoming Lessons</h2>
-            <ul>
-              {lessonList.map(lesson => (
-                <ListGroup.Item key={teacher._id}>
-                  <Link key={lesson._id} to={`/lesson/${lesson._id}/view`}>
-                    <h4>
-                      {lesson._teacher}
-                      {lesson.instrument}
-                      {lesson.hoursOfStudy}
-                      {lesson.status}
-                    </h4>
-                  </Link>
-                </ListGroup.Item>
-              ))}
-            </ul>
+            {lessonList.map(lesson => (
+              <ListGroup.Item key={lesson._id}>
+                <Link key={lesson._id} to={`/lesson/${lesson._id}/view`}>
+                  <h4>
+                    {lesson._teacher.name}
+                    {lesson.instrument}
+                    {lesson.hoursOfStudy}
+                    {lesson.status}
+                  </h4>
+                </Link>
+              </ListGroup.Item>
+            ))}
           </ListGroup>
         </div>
       </div>
